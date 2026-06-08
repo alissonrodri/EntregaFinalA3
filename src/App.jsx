32707343315beb5957837911b5a123230cd4aa9e
João Gama <jvg.dsantana@gmail.com>
2026-06-07 21:54:48 -0300

    atualização do código App.jsx, importando o arquivo recuperarSenha e adicionando a rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
+import RecuperarSenha from './pages/signIn/recuperarSenha';
 import SearchPage from './pages/search';
 import GamePage from './pages/gamepage';
 import CategoriesPage from './pages/categories';
 import CartPage from './pages/cart';
-import Wishlist from './pages/wishlist'
-import Library from './pages/library'
-import Checkout from './pages/checkout'
-import History from './pages/history'
+import Wishlist from './pages/wishlist';
+import Library from './pages/library';
+import Checkout from './pages/checkout';
+import History from './pages/history';
 
-function AppRoutes(){
+function AppRoutes() {
   const location = useLocation();
 
-  const rotasSemNavbar = ['/signin', '/signup'];
-  const rotasSemFooter = ['/signin', '/signup'];
+  // Adicionado '/recuperar-senha' aqui para esconder Navbar/Footer
+  const rotasSemLayout = ['/signin', '/signup', '/recuperar-senha'];
 
-  const rotasComNavbar = !rotasSemNavbar.includes(location.pathname);
-  const rotasComFooter = !rotasSemFooter.includes(location.pathname);
+  const rotasComNavbar = !rotasSemLayout.includes(location.pathname);
+  const rotasComFooter = !rotasSemLayout.includes(location.pathname);
 
-  return(
+  return (
     <>
-      {/* A Navbar só será renderizada se a rota atual não for login ou cadastro */}
       {rotasComNavbar && <Navbar />}
 
       <main>
@@ -32,6 +32,7 @@ function AppRoutes(){
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/recuperar-senha" element={<RecuperarSenha />} /> {/* Nova Rota */}
           <Route path="/search" element={<SearchPage />} />
           <Route path="/game/:id" element={<GamePage />} />
           <Route path="/categorias" element={<CategoriesPage />} />
@@ -44,20 +45,16 @@ function AppRoutes(){
       </main>
 
       {rotasComFooter && <Footer />}
-
-
     </>
   );
 }
 
 function App() {
- 
   return (
     <BrowserRouter>
-    <AppRoutes/>
+      <AppRoutes />
     </BrowserRouter>
-  )
-   
+  );
 }
 
-export default App
+export default App;
