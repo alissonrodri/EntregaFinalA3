eca9f6cddc9cc0b7a8d0e0847ff604cbdc6bc0c9
alissonrodri <alisson.rodrigo1307@gmail.com>
2026-06-17 10:30:30 -0300

    refatoração do rodapé

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,12 @@
 import './index.css';
+import { Link } from "react-router-dom";
+
 
 function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-content">
 
-        {/* Coluna 1 do rodapé */}
         <div className="footer-brand">
           <h2 className="footer-logo">CLT <span>Gaming</span></h2>
           <p className="footer-description">
@@ -13,29 +14,20 @@ function Footer() {
           </p>
         </div>
 
-        {/* Coluna 2 Links */}
-        <div className="footer-links">
-          <h3>Links Rápidos</h3>
-          <ul>
-            <li><a href="#lancamentos">Lançamentos</a></li>
-            <li><a href="#ofertas">Ofertas</a></li>
-            <li><a href="#assinatura">Assinatura</a></li>
-            <li><a href="#suporte">Suporte</a></li>
-          </ul>
-        </div>
-
-        {/* Coluna 3: Institucional */}
         <div className="footer-links">
           <h3>Institucional</h3>
-          <ul>
-            <li><a href="#sobre">Sobre nós</a></li>
-            <li><a href="#termos">Termos de serviço</a></li>
-            <li><a href="#privacidade">Política de privacidade</a></li>
-            <li><a href="#reembolso">Política de reembolso</a></li>
+          <ul>      
+            <li><Link to="/sobre">Sobre nós</Link></li>
+            <li><Link to="/termos">Termos de Serviço</Link></li>
+            <li><Link to="/privacidade">Política de Privacidade</Link></li>
+            <li><Link to="/reembolso">Política de Reembolso</Link></li>
+            <li><Link to="/faq">Perguntas Frequentes</Link></li>
+
           </ul>
         </div>
-      </div>
+       </div>
 
+       
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} CLT Gaming. Todos os direitos reservados.</p>
         <div className="footer-socials">
